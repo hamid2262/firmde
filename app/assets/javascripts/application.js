14e5db38197dd4b8619a87c5d0f91d4b320f3cb0
hamid <hamid2262@yahoo.com>
2014-10-21 17:53:02 +0200

    home page complete

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,16 +1,41 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
-// or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// compiled file.
-//
-// Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
-// about supported directives.
-//
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
+//= require my_theme/global/plugins/jquery-migrate.min.js
+//= require bootstrap-sprockets
+
+//= require my_theme/global/plugins/fancybox/source/jquery.fancybox.pack.js
+//= require my_theme/global/plugins/carousel-owl-carousel/owl-carousel/owl.carousel.min.js
+//= require my_theme/global/plugins/slider-revolution-slider/rs-plugin/js/jquery.themepunch.revolution.min.js
+//= require my_theme/global/plugins/slider-revolution-slider/rs-plugin/js/jquery.themepunch.tools.min.js
+//= require my_theme/frontend/pages/scripts/revo-slider-init.js
+
+//= require my_theme/frontend/layout/scripts/back-to-top.js
+//= require my_theme/frontend/pages/scripts/layerslider-init.js
+//= require my_theme/frontend/layout/scripts/layout.js
+
 //= require_tree .
+
+$(function() {
+  // Layout.init();    
+  // Layout.initOWL();
+  // LayersliderInit.initLayerSlider();
+  // Layout.initImageZoom();
+  // Layout.initTouchspin();
+  // RevosliderInit.initRevoSlider();
+  // Layout.initTwitter();
+    Layout.init();    
+    Layout.initOWL();
+    RevosliderInit.initRevoSlider();
+    Layout.initTwitter();
+    //Layout.initFixHeaderWithPreHeader(); /* Switch On Header Fixing (only if you have pre-header) */
+    //Layout.initNavScrolling(); 
+
+});
+
+// require my_theme/global/plugins/jquery-slimscroll/jquery.slimscroll.min.js
+// require my_theme/global/plugins/zoom/jquery.zoom.min.js
+// require my_theme/global/plugins/bootstrap-touchspin/bootstrap.touchspin.js
+// require my_theme/global/plugins/slider-layer-slider/js/greensock.js
+// require my_theme/global/plugins/slider-layer-slider/js/layerslider.transitions.js
+// require my_theme/global/plugins/slider-layer-slider/js/layerslider.kreaturamedia.jquery.js
