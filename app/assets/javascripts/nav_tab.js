7601fdd804c6bd3e3c983f8f4611fc1c121ab463
Marcel Witt <wittm@fh-trier.de>
2014-12-16 12:59:55 +0100

    nav_tab support für 5 tabs

--- app/assets/javascripts/nav_tab.js
+++ app/assets/javascripts/nav_tab.js
@@ -2,16 +2,38 @@
   $('.tab_1').addClass('active');
   $('.tab_2').removeClass('active');
   $('.tab_3').removeClass('active');
+  $('.tab_4').removeClass('active'); 
+  $('.tab_5').removeClass('active'); 
 });
 
  $('.nav_link_2').click(function(event){
   $('.tab_1').removeClass('active');
   $('.tab_2').addClass('active');
   $('.tab_3').removeClass('active');
+  $('.tab_4').removeClass('active');
+  $('.tab_5').removeClass('active');  
 });
 
  $('.nav_link_3').click(function(event){
   $('.tab_1').removeClass('active');
   $('.tab_2').removeClass('active');
   $('.tab_3').addClass('active');
+  $('.tab_4').removeClass('active'); 
+  $('.tab_5').removeClass('active'); 
+});
+
+ $('.nav_link_4').click(function(event){
+  $('.tab_1').removeClass('active');
+  $('.tab_2').removeClass('active');
+  $('.tab_3').removeClass('active');
+  $('.tab_4').addClass('active'); 
+  $('.tab_5').removeClass('active'); 
+});
+
+ $('.nav_link_5').click(function(event){
+  $('.tab_1').removeClass('active');
+  $('.tab_2').removeClass('active');
+  $('.tab_3').removeClass('active');
+  $('.tab_4').removeClass('active'); 
+  $('.tab_5').addClass('active'); 
 });
