fe40a93843bd58d26ed2bacf59079e10765578df
hamid <hamid2262@yahoo.com>
2015-01-12 14:00:42 +0100

    static_pages:contact uschange statick conyent to block

--- app/models/kunden.rb
+++ app/models/kunden.rb
@@ -1,5 +1,6 @@
 class Kunden < ActiveRecord::Base
 
+  after_save :clear_cache
 
   has_attached_file :photo, :styles => { :thumb => "159x101>" }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :photo, :content_type => /\Aimage\/.*\Z/
@@ -7,4 +8,8 @@ class Kunden < ActiveRecord::Base
 
   has_attached_file :photo_gray, :styles => { :thumb => "159x101>" }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :photo_gray, :content_type => /\Aimage\/.*\Z/  
+
+  def clear_cache
+    Rails.cache.clear
+  end
 end
