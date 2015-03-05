b1ea8e5a8e6bff1b1d99c992d7e0f0acefe9f232
hamid <hamid2262@yahoo.com>
2015-03-05 11:07:08 +0100

    remove sub page translation migrations

--- app/models/sub_page.rb
+++ app/models/sub_page.rb
@@ -1,6 +1,6 @@
 class SubPage < ActiveRecord::Base
 
-  translates :title, :seo_title, :slug, :body 
+  # translates :title, :seo_title, :slug, :body 
   belongs_to :page
 
   def to_param
--- db/migrate/20150304011943_translate_sub_pages.rb
+++ db/migrate/20150304011943_translate_sub_pages.rb
@@ -1,16 +1,16 @@
 class TranslatePages < ActiveRecord::Migration
-  def up
-    Page.create_translation_table!({
-      :title => :string,
-      :seo_title => :string,
-      :slug => :string,
-      :body => :text
-    }, {
-      :migrate_data => true
-    })
-  end
+  # def up
+  #   Page.create_translation_table!({
+  #     :title => :string,
+  #     :seo_title => :string,
+  #     :slug => :string,
+  #     :body => :text
+  #   }, {
+  #     :migrate_data => true
+  #   })
+  # end
 
-  def down
-    Page.drop_translation_table! :migrate_data => true
-  end
+  # def down
+  #   Page.drop_translation_table! :migrate_data => true
+  # end
 end
